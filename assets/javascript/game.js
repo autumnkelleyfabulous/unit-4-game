d44a380e81e4cc4342e016fbf264c5d3cd795269
Autumn Kelley Barr <autumnkelleyfabulous@Autumns-iMac.local>
2018-12-13 22:24:15 -0700

    changed code order

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,87 +1,142 @@
+var randomNumber;
+var wins = 0;
+var losses = 0;
+var computerNumber=  randomMath();
+var yourTotal = 0;
+var finalScore;
+
+var crystal1 = 0;
+var crystal2 = 0;
+var crystal3 = 0;
+var crystal4 = 0;
+
+function reset() {
+    
+    yourTotal= 0;
+    $(finalScore).text(yourTotal);
+  }
+
+  function won(){
+    alert("you win!");
+    wins++
+    $("#numberWins").text(wins);
+    reset();
+}
+
+function lost (){
+    alert('you lost!');
+    losses++;
+    $("#numberLosses").text(losses);
+}
+
+function randomMath() {
+    randomNumber = Math.floor(Math.random() * 120);
+    console.log;(randomNumber);
+    return randomNumber;
+
+} 
 
 $(document).ready(function() {
-    var randomNumber;
-    var losses;
+   
+// random number from computer
 
-    randomNumber;= Math.floor(math.random()* 99-30);
-    console.log;(randomNumber);
-    $("#result").html('random number:');
-
-    for(var i = 0; i < 4; i++){
-        var random = Math.floor(Math.random() *12
-        console.log(random);
-
-
-        var crystal1= $(<"div">);
-            button.attr({
-                "class:" "crystal1",
-                "data-random": randomNumber
-
-            });
-
-        
-        $(".btn-crystal1").append(crystal1);
-        
-    }
-
-    $('.btn-crystal1').append(crystal1);
-
-
-    .buttons
-
-    $("buttons").on("click", function(){
+    $(".crystal1").on("click", function(){
         console.log(this);
-        //  yourTotal = yourTotal + crystal1;
-        //  $("#yourTotal").html(finalScore);
+         yourTotal = yourTotal + crystal1;
+         $("#yourTotal").text(finalScore);
         
-        //  if(yourTotal == randomNumber) {
-        //      celebrate ();
-        //  }
-        //  else if ( yourTotal > randomNumber) {
-        //      lost();
+         if(yourTotal == randomNumber) {
+             celebrate ();
+         }
+         else if ( yourTotal > randomNumber) {
+             lost();
          
-        // }
+        }
+    });
         
-        // $("crystal2").on("click", function() {
-        //     yourTotal = yourTotal + crystal2;
-        //     $("#yourTotal").html(finalScore);
-           
-        //     if(yourTotal == randomNumber) {
-        //         celebrate ();
-        //     }
-        //     else if ( yourTotal > randomNumber) {
-        //         lost();
-        //     }
+    $(".crystal2").on("click", function() {
+        yourTotal = yourTotal + crystal2;
+        $("#yourTotal").text(finalScore);
         
-        // $("crystal3").on("click", function() {
-        //     yourTotal = yourTotal + crystal3;
-        //     $("#yourTotal").html(finalScore);
-           
-        //     if(yourTotal == randomNumber) {
-        //         celebrate ();
-        //     }
-        //     else if ( yourTotal > randomNumber) {
-        //         lost();
-        //     }
+        if(yourTotal == randomNumber) {
+            celebrate ();
+        }
+        else if ( yourTotal > randomNumber) {
+            lost();
+        }
+    });
         
-        // $("crystal4").on("click", function() {
-        //     yourTotal = yourTotal + crystal4;
-        //     $("#yourTotal").html(finalScore);
-           
-        //     if(yourTotal == randomNumber) {
-        //         celebrate ();
-        //     }
-        //     else if ( yourTotal > randomNumber) {
-        //         lost();
-        //     }
-        // }
+    $(".crystal3").on("click", function() {
+        yourTotal = yourTotal + crystal3;
+        $("#yourTotal").text(finalScore);
+        
+        if(yourTotal == randomNumber) {
+            celebrate ();
+        }
+        else if ( yourTotal > randomNumber) {
+            lost();
+        }
+    });
+        
+    $(".crystal4").on("click", function() {
+        yourTotal = yourTotal + crystal4;
+        $("#yourTotal").text(finalScore);
+        
+        if(yourTotal == randomNumber) {
+            celebrate ();
+        }
+        else if ( yourTotal > randomNumber) {
+            lost();
+        }
+    });
 
 
+    // for ( var i = 0; i < 4; i++){ 
+
+        crystal1 =
+        Math.floor(Math.random()* 12) +1;
+        crystal2 =
+        Math.floor(Math.random() * 12) +1;
+        crystal3 =
+        Math.floor(Math.random() *12) +1;
+        crystal4 =
+        Math.floor(Math.random() *12) +1;
+
+        $("#numberWins").text(wins);
+        $("#numberLosses").text(losses);
+
+        won()
+});
+
+    
+    
+    
+    
+    
+
+        //     var random = Math.floor(Math.random() *12
+        //     console.log(random);
+
+    // $("#result").html('random number:');
+
+    //
+    //     var crystal1= $(<"div">);
+    //         button.attr({
+    //             "class:" "crystal1",
+    //             "data-random": randomNumber
+
+    //         });
+
+    //     $(".btn-crystal1").append(crystal1);
+        
+    // }
+
+    // $('.btn-crystal1').append(crystal1);
+// buttons
+   
 
 
-//     $(randomNumber).text(Random);
-
-
+    // $(randomNumber).text(Random);
 
 
 
@@ -91,32 +146,9 @@ $(document).ready(function() {
 // var crystal4= Math.floor(Math.random()*12)
 
 
-
-// $("#numberWins").text(wins);
-// $("#numberLosses").text(losses);
-
-// function reset() {
 // Random=Math.floor(Math.random()*101+19);
 // console.log(random)
 // crystal1= Math.floor(Math.random()*1+11);
 // crystal2= Math.floor(Math.random()*1+11);
 // crystal3= Math.floor(Math.random()*1+11);
 // crystal4= Math.floor(Math.random()*1+11);
-//   yourTotal= 0;
-//   $(finalScore).text(yourTotal);
-// }
-
-
-// function celebrate (){
-//     alert("you win!");
-//     wins++
-//     $("#numberWins").text(wins);
-//     reset ();
-// }
-
-// function lost (){
-//     alert('you lost!');
-//     losses++;
-//     $("#numberLosses").text(losses);
-// }
-// 
